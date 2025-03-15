b6aa0462d2a99cee391c4b7c828b74e0c7f7602d
Mohammad Ahmad <mohammad.h.ahmad@gmail.com>
2025-03-15 20:00:20 +0200

    modifying default migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,8 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
     ];
@@ -30,7 +31,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -41,7 +41,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
